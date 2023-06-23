561d3e83864cc1ebf43b3ffe32f810b3ba9bb50a
IlyaSibogatov <ilya.sibogatov@gmail.com>
2023-06-23 16:12:16 +0300

    Added: - Deleting messages - Editing messages - Save state from edited messages

--- src/main/kotlin/com/example/data/source/MessageDataSourceImpl.kt
+++ src/main/kotlin/com/example/data/source/MessageDataSourceImpl.kt
@@ -3,6 +3,7 @@ package com.example.data.source
 import com.example.data.model.chat.Message
 import org.litote.kmongo.coroutine.CoroutineCollection
 import org.litote.kmongo.coroutine.CoroutineDatabase
+import org.litote.kmongo.eq
 
 class MessageDataSourceImpl(
     private val db: CoroutineDatabase
@@ -20,5 +21,20 @@ class MessageDataSourceImpl(
     override suspend fun insertMessage(chatId: String, msg: Message) {
         messages = db.getCollection<Message>(collectionName = chatId)
         messages.insertOne(msg)
+    }
+
+    override suspend fun editMessage(chatId: String, id: String, msg: String) {
+        messages = db.getCollection<Message>(collectionName = chatId)
+        val message = messages.find(Message::id eq id).toList().first()
+        message.apply {
+            this.wasEdit = this.message != msg
+            this.message = msg
+        }
+        messages.updateOne(Message::id eq id, message)
+    }
+
+    override suspend fun removeMessage(chatId: String, id: String) {
+        messages = db.getCollection<Message>(collectionName = chatId)
+        messages.deleteOne(Message::id eq id)
     }
 }
