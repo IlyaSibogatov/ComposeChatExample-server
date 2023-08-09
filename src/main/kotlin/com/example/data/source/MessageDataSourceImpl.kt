1ac71d2f998ea8c21d9a8d776887ab40013c123c
IlyaSibogatov <ilya.sibogatov@gmail.com>
2023-08-09 11:43:00 +0300

    - Using UID to get the user name in the message and the room owner - When a new user enters the chat room, they are added as a member

--- src/main/kotlin/com/example/data/source/MessageDataSourceImpl.kt
+++ src/main/kotlin/com/example/data/source/MessageDataSourceImpl.kt
@@ -1,7 +1,9 @@
 package com.example.data.source
 
 import com.example.data.model.chat.Message
-import org.litote.kmongo.coroutine.CoroutineCollection
+import com.example.data.model.chat.MessageHistory
+import com.example.data.model.user.User
+import com.example.data.model.user.UserChatInfo
 import org.litote.kmongo.coroutine.CoroutineDatabase
 import org.litote.kmongo.eq
 
@@ -9,32 +11,74 @@ class MessageDataSourceImpl(
     private val db: CoroutineDatabase
 ) : MessageDataSource {
 
+    private var messageHistory = db.getCollection<MessageHistory>()
+    private val users = db.getCollection<User>()
 
-    private lateinit var messages: CoroutineCollection<Message>
+
+    override suspend fun addFollower(chatId: String, uid: String) {
+        val currentHistory = messageHistory.find(MessageHistory::name eq chatId).first()
+        val users = currentHistory?.users?.toMutableList() ?: mutableListOf()
+        if (!users.contains(uid)) {
+            users.add(uid)
+            currentHistory?.let {
+                it.users = users
+                messageHistory.updateOne(MessageHistory::name eq chatId, currentHistory)
+            }
+        }
+    }
 
     override suspend fun getAllMessages(chatId: String): List<Message> {
-        messages = db.getCollection<Message>(collectionName = chatId)
-        return messages.find()
-            .descendingSort(Message::timestamp).toList()
+        val currentHistory = messageHistory.find(MessageHistory::name eq chatId).first()
+        val messages = currentHistory?.messages
+        return messages?.sortedByDescending { it.timestamp } ?: emptyList()
+    }
+
+    override suspend fun getFollowers(chatId: String): List<UserChatInfo> {
+        val chatUsers = messageHistory.find(MessageHistory::name eq chatId).first()?.users
+        val followers = chatUsers?.map { it ->
+            users.find(User::id eq it).first().let { user ->
+                UserChatInfo(
+                    user?.id ?: "",
+                    user?.username ?: "",
+                )
+            }
+        }
+        return followers ?: emptyList()
     }
 
     override suspend fun insertMessage(chatId: String, msg: Message) {
-        messages = db.getCollection<Message>(collectionName = chatId)
-        messages.insertOne(msg)
+        val currentHistory = messageHistory.find(MessageHistory::name eq chatId).first()
+        var messages = currentHistory?.messages
+        messages = messages?.plus(msg) ?: emptyList<Message>().plus(msg)
+        currentHistory?.messages = messages
+        if (currentHistory != null) {
+            messageHistory.updateOne(MessageHistory::name eq chatId, currentHistory)
+        }
     }
 
     override suspend fun editMessage(chatId: String, id: String, msg: String) {
-        messages = db.getCollection<Message>(collectionName = chatId)
-        val message = messages.find(Message::id eq id).toList().first()
-        message.apply {
-            this.wasEdit = this.message != msg
-            this.message = msg
+        val currentHistory = messageHistory.find(MessageHistory::name eq chatId).first()
+        val messages = currentHistory?.messages
+        messages?.find { it.id == id }?.let {
+            it.wasEdit = it.message != msg
+            it.message = msg
         }
-        messages.updateOne(Message::id eq id, message)
+        if (messages != null) {
+            currentHistory.messages = messages
+        }
+        if (currentHistory != null) {
+            messageHistory.updateOne(MessageHistory::name eq chatId, currentHistory)
+        }
     }
 
     override suspend fun removeMessage(chatId: String, id: String) {
-        messages = db.getCollection<Message>(collectionName = chatId)
-        messages.deleteOne(Message::id eq id)
+        val currentHistory = messageHistory.find(MessageHistory::name eq chatId).first()
+        val messageForRemove = currentHistory?.messages?.find { it.id == id }
+        val messages = currentHistory?.messages?.toMutableList()
+        messages?.remove(messageForRemove)
+        currentHistory?.messages = messages?.toList() ?: listOf()
+        if (currentHistory != null) {
+            messageHistory.updateOne(MessageHistory::name eq chatId, currentHistory)
+        }
     }
 }
