6b867eb7d787d4324cc640c6a57934d4d59d3f3b
GansAlles <iliy_siboganov@mail.ru>
2023-09-21 12:54:23 +0300

    Add more info in notification data

--- src/main/kotlin/com/example/firebase/FirebaseFcmMessaging.kt
+++ src/main/kotlin/com/example/firebase/FirebaseFcmMessaging.kt
@@ -1,6 +1,7 @@
 package com.example.firebase
 
 import com.example.data.model.NotificationModel
+import com.example.data.model.NotificationType
 import com.example.utils.Constants.FAILED_TAG
 import com.example.utils.Constants.SUCCESS_TAG
 import com.google.firebase.messaging.FirebaseMessaging
@@ -8,13 +9,25 @@ import com.google.firebase.messaging.Message
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
-suspend fun sendMessage(notification: NotificationModel): Boolean {
+suspend fun sendMessage(
+    notification: NotificationModel,
+    id: String,
+    type: NotificationType,
+    senderId: String,
+    senderName: String,
+): Boolean {
     var result = false
     withContext(Dispatchers.IO) {
         println("TOKEN IS ---> ${notification.token}")
         val messageData = Message.builder()
             .putData("title", notification.title)
             .putData("body", notification.body)
+
+            .putData("id", id)
+            .putData("type", type.value)
+            .putData("senderId", senderId)
+            .putData("senderName", senderName)
+
             .setToken(notification.token)
         result = try {
             FirebaseMessaging.getInstance().send(messageData.build())
