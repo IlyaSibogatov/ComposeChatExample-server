d0e1cbee24d1179bfc9284a924d6f99d76f231e0
GansAlles <iliy_siboganov@mail.ru>
2023-11-20 19:12:35 +0300

    Added: - Method for getting media descriptions

--- src/main/kotlin/com/example/data/controllers/MediaController.kt
+++ src/main/kotlin/com/example/data/controllers/MediaController.kt
@@ -1,10 +1,13 @@
 package com.example.data.controllers
 
+import com.example.data.model.sources.MediaDescription
 import com.example.data.source.MediaSource
 
 class MediaController(
     private val mediaSource: MediaSource,
 ) {
+    suspend fun getMediaDescription(uuid: String, mediaId: String, type: String): MediaDescription? =
+        mediaSource.getMediaDescription(uuid, mediaId, type)
     suspend fun addPhoto(uuid: String, description: String, isAvatar: Boolean): String =
         mediaSource.addPhoto(uuid, description, isAvatar)
 
--- src/main/kotlin/com/example/data/source/MediaSource.kt
+++ src/main/kotlin/com/example/data/source/MediaSource.kt
@@ -1,6 +1,10 @@
 package com.example.data.source
 
+import com.example.data.model.sources.MediaDescription
+
 interface MediaSource {
+
+    suspend fun getMediaDescription(uuid: String, mediaId: String, type: String): MediaDescription?
 
     suspend fun addPhoto(uuid: String, description: String, isAvatar: Boolean): String
 
