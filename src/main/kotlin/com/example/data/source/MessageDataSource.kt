6321629c6c95e1a786132c37daff64d435ea6e1b
IlyaSibogatov <ilya.sibogatov@gmail.com>
2023-11-20 19:12:35 +0300

    Added: - Method for getting media descriptions

--- src/main/kotlin/com/example/data/source/MessageDataSource.kt
+++ src/main/kotlin/com/example/data/source/MessageDataSource.kt
@@ -6,8 +6,11 @@ import com.example.data.model.user.UserChatInfo
 interface MessageDataSource {
 
     suspend fun getAllMessages(chatId: String): List<Message>?
+
     suspend fun insertMessage(chatId: String, msg: Message)
+
     suspend fun editMessage(chatId: String, id: String, msg: String)
+
     suspend fun removeMessage(chatId: String, id: String)
 
     suspend fun getFollowers(chatId: String): List<UserChatInfo>
